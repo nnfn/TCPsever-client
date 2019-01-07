fed6b48e4e85a1130a910bb2528a8a99b8b5eee7
nnFn <44121977+nnfn@users.noreply.github.com>
2019-01-07 22:57:23 +0900

    Update tcpclient.py
    チャットできるようにした

--- tcpclient.py
+++ tcpclient.py
@@ -1,12 +1,16 @@
+# -*- coding: utf-8 -*-
 import socket
 
-thost = "127.0.0.1"
+user = input(">> ")
+
+thost = user
 tport =  9999
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((thost,tport))
-msg = input(">>> ")
-msg = msg.encode()
-client.send(msg)
-response = client.recv(1024)
-print(response)
+while True:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((thost,tport))
+    msg = input(">>> ")
+    msg = msg.encode("utf-8")
+    client.send(msg)
+    response = client.recv(1024)
+    print(response)
