8365b103924a126185c825da0ae0b91559a2ca15
nnFn <44121977+nnfn@users.noreply.github.com>
2019-01-11 06:51:16 +0900

    Update tcpclient.py

--- tcpclient.py
+++ tcpclient.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import socket
 
-user = input(">> ")
+user = input("サーバー側のIPを入力>> ")
 
 thost = user
 tport =  9999
@@ -9,8 +9,7 @@ tport =  9999
 while True:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((thost,tport))
-    msg = input(">>> ")
-    msg = msg.encode("utf-8")
-    client.send(msg)
-    response = client.recv(1024)
-    print(response)
+    msg = input(">> ")
+    client.send(msg.encode("utf-8"))
+    res = client.recv(1024)
+    print(res.decode("utf-8"))
