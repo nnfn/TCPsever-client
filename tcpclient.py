0b20c0a59f6e60d8d33decde7f9cfb64d234e094
nnFn <44121977+nnfn@users.noreply.github.com>
2019-01-15 23:54:57 +0900

    Update tcpclient.py

--- tcpclient.py
+++ tcpclient.py
@@ -7,9 +7,13 @@ thost = user
 tport =  9999
 
 while True:
-  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  client.connect((thost,tport))
-  msg = input(">> ")
-  client.send(msg.encode("utf-8"))
-  res = client.recv(1024)
-  print(res.decode("utf-8"))
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((thost,tport))
+    msg = input(">> ")
+    if msg == "exit":
+        client.close()
+        break
+    client.send(msg.encode("utf-8"))
+    res = client.recv(1024)
+    print(res.decode("utf-8"))
+
